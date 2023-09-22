d41d9d466bd1b7d2224f92c282fb7cb1e3c2b8d1
Shagomir <m.j.knudson@gmail.com>
2023-09-21 19:52:07 -0500

    updating JS to use more functions

--- script.js
+++ script.js
@@ -13,53 +13,62 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+function getLength() {
+  // ask user for length. 
+  var l = prompt("Please enter the length of the password required, between 8 and 128 characters", 8)
+  // ask again until they provide a value between 8 and 128
+  while (l < 8 || l > 128) {
+    l = prompt("The length must be between 8 and 128 characters, please enter a valid value", 8)
+  };
+  return l
+}
+
+function getParams() {
+  //get character options for password. We assume we always want lowercase.
+  return [true, 
+    confirm("Would you like to use uppercase letters in your password?\nPress 'Cancel' to generate a password without Uppercase letters."),
+    confirm("Would you like to use numbers in your password?\nPress 'Cancel' to generate a password without numbers."),
+    confirm("Would you like to use symbols in your password?\nPress 'Cancel' to generate a password without symbols.")]
+}
+
+function getSet() {
+  //confirm set of characters to use
+  if (confirm("Would you like to remove commonly confused characters?\nPress 'Cancel' to generate a password using the full set of characters and symbols.")) {
+    return ["abcdefghijkmnopqrstuvwxyz", "ABCDEFGHJKLMNPRSTUVWXYZ", "23456789", ":=?@!#%+"]    //  a simplified set of characters for easier to type passwords. less secure! 
+  }
+  return ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]  // larger set of allowed characters for more secure passwords. 
+}
+
+function generateCharacters(pl, par, set) {
+  //generates as set of characters for the password which will include all of the types that are required
+  var chars = "";
+  var result = "";
+  for (var i = 0; i < par.length; i++) {
+    if (par[i]) {
+      result += set[i].charAt(Math.floor(Math.random() * set[i].length))
+      chars += set[i]
+    }
+  }
+  //pad out the password length with additional random characters from the set.
+  var req = result.length
+  for (var i = 0; i < (pl - req); i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result
+}
+
+function shuffle(string) {
+  // Shuffle result using Fisher-Yates shuffle to ensure special symbols are not at the beginning and return the finished password.
+  // https://stackoverflow.com/questions/3943772/how-do-i-shuffle-the-characters-in-a-string-in-javascript
+  for(var i = 0; i < string.length; i++){
+      var j = Math.floor(Math.random()*string.length)
+      var k = string[j] 
+      string[j] = string[i]
+      string[i] = k
+  }
+  return string
+}
 
 function generatePassword() {
-  // ask user for parameters. 
-  var passLength = prompt("Please enter the length of the password required, between 8 and 128 characters", 8)
-  // ask again until they provide a value between 8 and 128
-  while (passLength < 8 || passLength > 128) {
-    passLength = prompt("Please enter a length value between 8 and 128 characters.", 8)
-  };
-  var useUppercase = confirm("Would you like to use uppercase letters in your password?\nPress 'Cancel' to generate a password without Uppercase letters.")
-  var useNumbers = confirm("Would you like to use numbers in your password?\nPress 'Cancel' to generate a password without numbers.")
-  var useSymbols = confirm("Would you like to use symbols in your password?\nPress 'Cancel' to generate a password without symbols.")
-  var simplifyPassword = confirm("Would you like to remove commonly confused characters?\nPress 'Cancel' to generate a password using the full set of characters and symbols.")
-
-  //  a simplified set of characters for easier to type passwords. less secure! 
-  if (simplifyPassword) {
-    var lowercase = "abcdefghijkmnopqrstuvwxyz";
-    var uppercase = "ABCDEFGHJKLMNPRSTUVWXYZ";
-    var numbers = "23456789";
-    var symbols = ":=?@!#%+";
-  } else {
-    // larger set of allowed characters for more secure passwords. 
-    var lowercase = "abcdefghijklmnopqrstuvwxyz";
-    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var numbers = "0123456789";
-    var symbols = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  }
-  // adding at least one of each type of character to result, depending on prompts.
-  var result = lowercase.charAt(Math.floor(Math.random() * lowercase.length))
-  if (useUppercase) { result += uppercase.charAt(Math.floor(Math.random() * uppercase.length)) }
-  if (useNumbers) { result += numbers.charAt(Math.floor(Math.random() * numbers.length)) }
-  if (useSymbols) { result += symbols.charAt(Math.floor(Math.random() * symbols.length)) }
-  // use the prompts to create a set of charaters for randomly generating the remaining characters. 
-  var characters = lowercase;
-  if (useUppercase) { characters += uppercase };
-  if (useNumbers) { characters += numbers };
-  if (useSymbols) { characters += symbols };
-
-  // calculate length of password and randomly generate remaining characters. 
-  // https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-  var currentPassLength = result.length
-  for (var i = 0; i < (passLength - currentPassLength); i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  };
-
-  // now shuffle result using Fisher-Yates shuffle to ensure special symbols are not at the beginning and return the finished password.
-  // https://stackoverflow.com/questions/3943772/how-do-i-shuffle-the-characters-in-a-string-in-javascript
-
-  return result.split("").map(v => [v, Math.random()]).sort((a, b) => a[1] - b[1]).map(v => v[0]).join("")
-
+  return shuffle(generateCharacters(getLength(), getParams(), getSet()))
 }
